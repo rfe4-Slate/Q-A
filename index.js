842aaf334486de267a9d6f5656649f2e79564ddd
Harshi <harshi.dandu88@gmail.com>
2021-08-14 14:16:05 -0500

    fixed start script

--- index.js
+++ index.js
@@ -5,3 +5,29 @@ const app = express();
 const axios = require('axios');
 const pool = require('./Database/index.js');
 
+app.use(express.json());
+
+//get
+
+app.get('/', (req, res) => {
+  res.status(200).send("Yay!");
+});
+//  /qa/questions
+//  /qa/questions/:question_id/answers
+
+
+//put
+//  /qa/questions/:question_id/helpful
+//  /qa/questions/:question_id/report
+//  /qa/answers/:answer_id/helpful
+//  /qa/answers/:answer_id/report
+
+
+//post
+// /qa/questions
+//  /qa/questions/:question_id/answers
+
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
