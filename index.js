e4ab20aeecb21071b862ac2e5107b1732598f86c
Harshi <harshi.dandu88@gmail.com>
2021-08-15 22:52:03 -0500

    add routes for get

--- index.js
+++ index.js
@@ -3,25 +3,38 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const axios = require('axios');
-const pool = require('./Database/index.js');
+const pool = require('./Database/db.js');
 
 app.use(express.json());
+
+const routeName = {
+  listQuestions: '/qa/questions',
+  listAnswers: '/qa/questions/:question_id/answers',
+  addQuestion: '/qa/questions'
+}
 
 //get
 
 app.get('/', (req, res) => {
-  res.status(200).send("Yay!");
+  console.log('req: ', req);
+  res.status(200).send("Yay! Get Works!");
 });
-//  /qa/questions
+
+
+// //  /qa/questions
+app.get('/qa/questions', (req, res) => {
+  res.status(200).send("Yay! GET /qa/questions works!");
+});
 //  /qa/questions/:question_id/answers
 
 
 //put
+
+//app.put()
 //  /qa/questions/:question_id/helpful
 //  /qa/questions/:question_id/report
 //  /qa/answers/:answer_id/helpful
 //  /qa/answers/:answer_id/report
-
 
 //post
 // /qa/questions
